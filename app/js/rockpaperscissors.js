7fb693e698840046295a3496ee7b82746d194fea
seanvsville <seanvanho@gmail.com>
2016-03-01 18:45:28 -0800

    Unit 3 Homework
    Unit 3 Homework:
    var move and if statements for player and computer

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,17 +22,25 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    var move
+    if (move != null){
+        move = move;
+    }
+    else {
+        move = getInput();
+    }
+    return move;
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    var move
+    if (move != null){
+        move = move;
+    }
+    else {
+        move = randomPlay();
+    }
+    return move;
 }
 
 function getWinner(playerMove,computerMove) {
