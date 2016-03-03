4ec7290205ad4322e1d994d98670d4b3f07dd78d
Sean Van Ho <seanvanho@gmail.com>
2016-03-03 11:37:31 -0800

    cleaned up else and if statements syntax

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -47,37 +47,37 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    if(playerMove == 'rock' && computerMove == 'scissors'){
+    if (playerMove == 'rock' && computerMove == 'scissors'){
     	winner = "player"
     	console.log("Player wins!");
     	}
     	
-    	else if(playerMove == 'paper' && computerMove == 'rock'){
+    	else if (playerMove == 'paper' && computerMove == 'rock'){
     	winner = "player"
     	console.log("Player wins!");
     	}
     	
-    	else if(playerMove == 'scissors' && computerMove == 'paper'){
+    	else if (playerMove == 'scissors' && computerMove == 'paper'){
     	winner = "player"
     	console.log("Player wins!");
     	}
     	
-    	else if(playerMove == 'rock' && computerMove == 'paper'){
+    	else if (playerMove == 'rock' && computerMove == 'paper'){
     	winner = "computer"
     	console.log("Computer wins!");
     	}
     	
-    	else if(playerMove == 'paper' && computerMove == 'scissors'){
+    	else if (playerMove == 'paper' && computerMove == 'scissors'){
     	winner = "computer"
     	console.log("Computer wins!");
     	}
     	
-    	else if(playerMove == 'scissors' && computerMove == 'rock'){
+    	else if (playerMove == 'scissors' && computerMove == 'rock'){
     	winner = "computer"
     	console.log("Computer wins!");
     	}
     	
-    	else{
+    	else {
     		winner = "tie"
     		console.log("It's a tie!");
     	}	
