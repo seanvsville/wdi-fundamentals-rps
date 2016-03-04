24ecd2e2fa369456ade88a73224e3f95378ee72f
Sean Van Ho <seanvanho@gmail.com>
2016-03-04 12:11:59 -0800

    completed playToFive function

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -47,17 +47,17 @@ function getWinner(playerMove,computerMove) {
     var winner;
     if (playerMove == 'rock' && computerMove == 'scissors') {
     	winner = "player";
-    	console.log("Player wins!");
+    	console.log("You won!");
     	}
     	
     	else if (playerMove == 'paper' && computerMove == 'rock') {
     	winner = "player";
-    	console.log("Player wins!");
+    	console.log("You won!");
     	}
     	
     	else if (playerMove == 'scissors' && computerMove == 'paper') {
     	winner = "player";
-    	console.log("Player wins!");
+    	console.log("You won!");
     	}
     	
     	else if (playerMove == 'rock' && computerMove == 'paper') {
@@ -87,15 +87,20 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    // This function should continue to play Rock Paper Scissors until either the
-    // player or the computer has won five times.
-    // After each 'round', display some text in the console indicating who played
-    // what, who won, and what the current scoreboard looks like.
-    // For example,
-    //  console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
-    //  console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
-    /* YOUR CODE HERE */
+    var winner = getWinner(playerMove, computerMove);
+    while (playerWins < 5 && computerWins < 5) {
+        if (winner === "player") {
+            playerWins += 1;
+        }
+        else if (winner === "computer") {
+            computerWins += 1;
+        }
+        console.log("You chose" + " " + playerMove + "while the computer chose" + " " + computerMove + ".");
+        console.log("The winner of this round is" + winner + "!");
+        console.log("The score is currently" + " " + playerWins + "to" + computerWins + ".\n");
+    }
+    getInput();
+    randomPlay();
     return [playerWins, computerWins];
 }
-
+    playToFive();
